dcb2c9ccbb76947721d3def19409b0d66471dcc2
TymQa <125615116+TymQa@users.noreply.github.com>
2023-06-30 11:53:47 +0300

    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function(event) {
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    if (emailInput.value === ''  || passwordInput.value === '') {
+        alert('Все поля должны быть заполнены')
+        return;
+    }
+    
+    const formData = {
+        email: emailInput.value,
+        password: passwordInput.value
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+
+});
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes')
+
+createButton.addEventListener('click', function() {
+
+  const input = document.querySelector('input[type="number"]');
+  const amount = Number(input.value);
+
+  createBoxes(amount);
+
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+
+  const boxSize = 30;
+  let html = '';
+
+  for (let i = 0; i < amount; i++) {
+    const size = boxSize + i * 10;
+    const color = getRandomHexColor();
+    html += `<div style="width: ${size}px; height: ${size}px; background-color: ${color}"></div>`;
+  }
+
+  boxesDiv.innerHTML = html;
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
